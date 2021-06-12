9bdbadef934ca79eb032db062cc9bb2b574bd64a
Aastha <34710946+aasthas97@users.noreply.github.com>
2021-06-12 16:04:02 +0530

    Update and rename getdata2.m to getData.m

--- bpl-functions/getData.m
+++ bpl-functions/getData.m
@@ -1,9 +1,9 @@
-function getdata2()
+function getData()
     for idx1 = 1 % skipped 8, 17, 20, 26, 28, 29_3, 30, 31_7, 32, 34, 36, 45, 47, 48
         for idx2 = 1
             diaryname = strcat(num2str(idx1), '_', num2str(idx2), '.txt');
             diary (diaryname)
-            filename = strcat('C:\Users\aasth\Documents\Thesis_new\Data\skeletons2\', num2str(idx1), '_', num2str(idx2), 'skel.png');
+            filename = strcat('./data/skeletons/', num2str(idx1), '_', num2str(idx2), 'skel.png');
             fprintf('\n%s\n', filename);
             im = imchange(filename); %convert to 105 x 105 logical
             fit_motorprograms(im, 10, true, false, true);
